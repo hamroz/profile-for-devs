e1f9541edc0b59f4859ba1c64221faadba9e133f
hamroz <hamroz.gavharov@gmail.com>
2021-11-09 15:29:53 +0500

    a

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def projects(request):
-    return render(request, "projects.html")
+    return render(request, "projects/projects.html")
+
 
 def project(request, pk):
-    return render(request, "single-projects.html")
+    return render(request, "projects/single-projects.html")
