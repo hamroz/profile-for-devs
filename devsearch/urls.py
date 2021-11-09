151f217cfd44c082070baaa367ada649069a2bca
hamroz <hamroz.gavharov@gmail.com>
2021-11-09 11:34:22 +0500

    proj

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,7 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse("Here are our products")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', projects)
 ]
