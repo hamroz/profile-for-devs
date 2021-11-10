8535f1f6d67ced3baafd985d3b233ee4088aa3cb
hamroz <hamroz.gavharov@gmail.com>
2021-11-10 21:08:47 +0500

    a

--- projects/views.py
+++ projects/views.py
@@ -36,11 +36,17 @@ def updateProject(request, pk):
         if form.is_valid():
             form.save()
             return redirect("projects")
-    
+
     context = {"form": form}
     return render(request, "projects/project_form.html", context)
 
+
 def deleteProject(request, pk):
-    
-    context = {}
-    return render(reqeus, "projects/delete-template", context)
+    project = Project.objects.get(id=pk)
+
+    if request.method == "POST":
+        project.delete()
+        return redirect("projects")
+
+    context = {"object": project}
+    return render(request, "projects/delete_template.html", context)
