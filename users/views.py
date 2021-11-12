90a91f5e2cb286cb26e6a860586965c9191c0466
hamroz <hamroz.gavharov@gmail.com>
2021-11-12 15:02:14 +0500

    a

--- users/views.py
+++ users/views.py
@@ -9,7 +9,10 @@ def profiles(request):
 
 
 def userProfile(request, pk):
-    profiles = Profile.objects.all()
-    context = {"profiles": profiles}
+    profile = Profile.objects.get(id=pk)
 
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {"profile": profile, "topSkills": topSkills, "otherSkills": otherSkills}
     return render(request, "users/user-profile.html", context)
