34cd558d0b2f81d9ab9b565501ae54c290c074ca
hamroz <hamroz.gavharov@gmail.com>
2021-11-15 19:45:22 +0500

    few adjusments

--- users/forms.py
+++ users/forms.py
@@ -7,12 +7,12 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["first_name", "email", "username", "password1", "password2"]
-        labels = {"first_name": "Name",}
-
+        labels = {
+            "first_name": "Name",
+        }
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        
+
         for name, field in self.fields.items():
-            field.widget.attrs.update({'class': 'input'})
-        
+            field.widget.attrs.update({"class": "input"})
