c43c626d61789a30dac492a39a88d5ff7d701028
hamroz <hamroz.gavharov@gmail.com>
2021-11-20 13:54:04 +0500

    a

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, ProfileForm
 
 
 def loginUser(request):
@@ -87,3 +87,9 @@ def userAccount(request):
     
     context = {"profile":profile, "skills": skills, "projects":projects}
     return render(request, "users/account.html", context)
+
+@login_required(login_url='login')
+def editAccount(request):
+    form = ProfileForm
+    context = {"form": form}
+    return render(request, "users/profile_form.html", context)
